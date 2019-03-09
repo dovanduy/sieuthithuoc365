03cf0cfe5cf04ee7a3c263ca39f91a6336770192
BuiXuanBach93 <donbach.it@gmail.com>
2019-03-09 21:33:05 +0700

    try to fix cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -50,6 +50,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        //other middlewares
+        'cors' => 'App\Http\Middleware\CORS',
         'admin' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ use Illuminate\Http\Request;
 //    return $request->user();
 // });
 
-Route::post('contact-submit','OysterGoldController@contactSubmit');
+Route::post('contact-submit','OysterGoldController@contactSubmit')->middleware('cors');
